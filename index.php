3ec981556ab684460b4866d0f628f402f7a580a5
Oguz <oguztopcu7@gmail.com>
2021-04-23 02:19:40 +0300

    get data from jsonplaceholder with guzzle

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo 'github actions';
+require 'vendor/autoload.php';
+
+use \GuzzleHttp\Client;
+
+try {
+	$request = new Client();
+
+	$response = $request->request(
+		'GET', 
+		'https://jsonplaceholder.typicode.com/posts'
+	);
+
+	if ($response->getStatusCode() != 200) {
+		throw new \RuntimeErrorException('Try to jsonplaceholder not respond');
+	}
+
+	echo '<ul>';
+	foreach (json_decode($response->getBody()->getContents()) as $item) {
+		echo "<li><a href=\"{$item->id}\">{$item->title}</a></li>";
+	}
+	echo '</ul>';
+} catch (\Exception $exception) {
+	echo $exception->getMessage();
+}
